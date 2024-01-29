0574d4c0a19c4586043c22bb5888a960aff74fc3
hashamhaq <155137509+hashamhaq@users.noreply.github.com>
2024-01-29 16:44:05 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5002;
 
-app.get('/', (req, res) => res.send('Frontend is live'));
+app.get('/', (req, res) => res.send('Frontend version 1'));
 
 app.listen(port, () => console.log(`App 1 listening on port ${port}`));
