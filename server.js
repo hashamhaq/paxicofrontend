16d913a02ab8fbdc230afb31715773d855287578
hashamhaq <155137509+hashamhaq@users.noreply.github.com>
2024-01-29 14:21:21 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 5002;
 
-app.get('/', (req, res) => res.send('Frontend file 3rd change through git 5002'));
+app.get('/', (req, res) => res.send('Frontend 1st change through GIT and container jenkins 5002'));
 
 app.listen(port, () => console.log(`App 1 listening on port ${port}`));
